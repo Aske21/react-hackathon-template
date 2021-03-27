4ff1071925d096ce10788d6d942b40ba6d1d8364
Aske21 <veledarevica@yahoo.com>
2021-03-27 18:30:59 +0100

    fixed login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,63 +6,66 @@ import { UserOutlined, LockOutlined } from "@ant-design/icons";
 function LoginPage(props) {
   return (
     <div className="wrapper">
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-      >
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Unesite vašu email adresu!",
-            },
-          ]}
+      <div className="content">
+        <h2 className="regtitle">Prijava</h2>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Vaša email adresa"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Vaša šifra"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Zapamti me</Checkbox>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Unesite vašu email adresu!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Vaša email adresa"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Vaša šifra"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Zapamti me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Zaboravili ste šifru?
+            </a>
           </Form.Item>
 
-          <a className="login-form-forgot" href="">
-            Zaboravili ste šifru?
-          </a>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            Prijavi se
-          </Button>
-          ili <a href="/register">Registruj se!</a>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Prijavi se
+            </Button>
+            ili <a href="/register">Registruj se!</a>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
