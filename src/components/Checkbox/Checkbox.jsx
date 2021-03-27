ff469cf90d898a2dc36d2ce5e02492c8d35bbc42
Makii117 <MakiOnPc@gmail.com>
2021-03-27 14:43:26 +0100

    ckeckbox and progressbar

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Checkbox } from 'antd';
+
+function onChange(e) {
+  console.log(`checked = ${e.target.checked}`);
+}
+
+ReactDOM.render(<Checkbox onChange={onChange}>Checkbox</Checkbox>, mountNode)
+
