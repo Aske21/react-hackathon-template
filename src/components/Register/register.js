aadb8ba00c08873a2f6066d146154838226669e6
Aske21 <veledarevica@yahoo.com>
2021-03-27 17:44:45 +0100

    fixed login, adding padding

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import { Form, Input, InputNumber, Button } from 'antd';
+import { Form, Input, InputNumber, Button } from "antd";
 
-import './register.css';
+import "./register.css";
 
 const layout = {
   labelCol: { span: 8 },
@@ -10,43 +10,58 @@ const layout = {
 
 /* eslint-disable no-template-curly-in-string */
 const validateMessages = {
-  required: '${label} is required!',
+  required: "${label} is required!",
   types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
   },
   number: {
-    range: '${label} must be between ${min} and ${max}',
+    range: "${label} must be between ${min} and ${max}",
   },
 };
 
 function Register(props) {
-
   return (
     <div className="wrapper">
       <div className="content">
-      <Form {...layout} name="nest-messages"  validateMessages={validateMessages}>
-        <Form.Item name={['user', 'name']} label="Ime" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'age']} label="Godine" rules={[{ type: 'number', min: 0, max: 99 }]}>
-          <InputNumber />
-        </Form.Item>
-        <Form.Item name={['user', 'website']} label="Website">
-          <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'introduction']} label="Introduction">
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+        <Form
+          {...layout}
+          name="nest-messages"
+          validateMessages={validateMessages}
+        >
+          <Form.Item
+            name={["user", "name"]}
+            label="Ime"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={["user", "email"]}
+            label="Email"
+            rules={[{ type: "email" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={["user", "age"]}
+            label="Godine"
+            rules={[{ type: "number", min: 0, max: 99 }]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item name={["user", "website"]} label="Website">
+            <Input />
+          </Form.Item>
+          <Form.Item name={["user", "introduction"]} label="Introduction">
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
