44c5f40b8781d2d7fd570ce16fd8f1b0a37388e5
Makii117 <MakiOnPc@gmail.com>
2021-03-27 17:09:46 +0100

    List and timeline updated

---
+++ src/components/Timeline/Timeline.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { Timeline, Radio } from 'antd';
+
+function TimelimeLabelDemo() {
+
+
+  return (
+    <>
+  
+    
+      <Timeline mode="left">
+        <Timeline.Item label="2015-09-01">Narudžba preuzeta</Timeline.Item>
+        <Timeline.Item label="2015-09-01 09:12:11">Nardužba kupljena</Timeline.Item>
+        <Timeline.Item label ="2021-27-03">Naružba u tranzitu</Timeline.Item>
+        <Timeline.Item label="2015-09-01 09:12:11">Narudžba dostavljena</Timeline.Item>
+      </Timeline>
+    </>
+  );
+}
+
+export default TimelimeLabelDemo
--- src/pages/Home/Progress/progress.js
+++ src/pages/Home/Progress/progress.js
@@ -6,7 +6,7 @@ import { Row, Col } from 'antd';
  import CheckboxCard from '../../../components/Card/Card';
 import ProgressBar from '../../../components/ProgressBar/ProgressBar';
 
-import TimelimeLabelDemo from '../../../../'
+import TimelimeLabelDemo from '../../../components/Timeline/Timeline.jsx'
 
 export const Progress = () => {
 
@@ -14,13 +14,13 @@ export const Progress = () => {
         <div className="progress">
         <Row>
             <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            <ProgressBar percent="10"/>
+            <TimelimeLabelDemo/>
             </Col>
             <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
             <CheckboxCard/>
             </Col>
             <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            Col
+            
             </Col>
         </Row>
 
